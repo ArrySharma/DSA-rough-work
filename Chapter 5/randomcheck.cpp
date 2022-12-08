282f2da7123c3ecf729a205a0a7f5f82acc8b030
Arry <realarshdeepsharma@gmail.com>
2022-12-08 03:22:33 -0400

    new files

---
+++ Chapter 5/randomcheck.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout << (17>>1) << endl;
+    cout << (17>>2) << endl;
+    cout << (19<<1) << endl;
+    cout << (21<<2) << endl;
+
+    int a, b = 1;
+    a = 10;
+    if(a-10)
+        cout << b;
+    else
+        cout << ++b;
+
+
+
+
+
+
+}
---
+++ forloops.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int n;
+    cout << "Enter the Value of N: " << endl;
+    cin >> n;
+
+    cout<< "printing count from 1 to n: " << endl;
+
+    for(int i = 1; i<=n; i = i + 2){
+        cout<< i << endl;
+    }
+
+
+
+
+}
