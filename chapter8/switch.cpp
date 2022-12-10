4b6ced89b14f57dd703a8e6567017b5bdf5cfb5d
Arry <realarshdeepsharma@gmail.com>
2022-12-10 01:06:35 -0400

    Switch Case

---
+++ chapter8/switch.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num = 6;
+
+    switch( num ){
+
+        case 1: cout << "First" << endl;
+                break;
+        
+        case 2: cout << "Second" << endl;
+                break;
+
+        default: cout << "It is a default case" << endl;
+        
+
+    }
+    cout << endl;
+
+    return 0;
+
+
+
+
+}
