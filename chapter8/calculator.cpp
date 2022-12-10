7dec42469c4ef68c497804fffbaaa509c5528fbf
Arry <realarshdeepsharma@gmail.com>
2022-12-10 02:05:41 -0400

    calculator

---
+++ chapter8/calculator.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+
+    cout << "Enter the value of a: " << endl;
+    cin >> a;
+
+    cout << "Enter the value of b: " << endl;
+    cin >> b;
+
+    char op;
+    cout << "Enter the operation: " ;
+    cin >> op;
+    cout << endl;
+
+    switch(op){
+
+        case '+': cout << (a+b) << endl;
+                    break;
+
+        case '-': cout << (a-b) << endl;
+                    break;
+
+        case '*': cout << (a*b) << endl;
+                    break;
+
+        case '/': cout << (a/b) << endl;
+                    break;
+
+        case '%': cout << (a%b) << endl;
+                    break;
+
+        default: cout << "Invalid opration" << endl;
+                    break;
+    }
+
+    return 0;
+}
---
+++ chapter8/whileswitch.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int num = 1;
+
+    while(1){
+
+        switch(num){
+
+            case 1: cout << num << endl;
+            break;
+            exit(1);
+        }
+        
+    }
+}
